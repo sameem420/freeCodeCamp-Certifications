c3e3310338ffbd36aaa2a145d4628174785d782c
Muhammad Sameem <sameemgujjar123@yahoo.com>
2024-02-26 14:36:52 +0400

    :heavy_check_mark: module Create readable code with conventions, whitespace, and comments in C#

---
+++ Foundational C# with Microsoft/Create readable code/variable-names/Program.cs
@@ -0,0 +1,9 @@
+﻿// example of good variable names
+
+char userOption;
+
+int gameScore;
+
+float particlesPerMillion;
+
+bool processedCustomer;
---
+++ Foundational C# with Microsoft/Create readable code/whitespaces/Program.cs
@@ -0,0 +1,49 @@
+﻿// Random dice = new Random();
+
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
+
+// int total = roll1 + roll2 + roll3;
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// {
+//     if ((roll1 == roll2) && (roll2 == roll3))
+//     {
+//         Console.WriteLine("You rolled triples!  +6 bonus to total!");
+//         total += 6;
+//     }
+//     else
+//     {
+//         Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+//         total += 2;
+//     }
+// }
+
+
+/* Coding Challenge
+//
+   This code reverses a message, counts the number of times 
+   a particular character appears, then prints the results
+   to the console window.
+//
+*/
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] message = originalMessage.ToCharArray();
+
+Array.Reverse(message);
+int letterCount = 0;
+
+foreach (char letter in message)
+{
+    if (letter == 'o')
+    {
+        letterCount++;
+    }
+}
+
+string newMessage = new String(message);
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
