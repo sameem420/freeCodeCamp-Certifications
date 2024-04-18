f8a0f0307e4ced3236cd01c2b64870a5cb0cb436
Muhammad Sameem <sameemgujjar123@yahoo.com>
2024-04-18 10:01:13 +0400

    :heavy_check_mark: challenge activity permission

---
+++ Foundational C# with Microsoft/Add logic to C# console applications/challenge-activity-permissions/Program.cs
@@ -0,0 +1,35 @@
+﻿// challenge activity using conditional operators
+// Decision logic challenge
+// In this challenge, you'll implement decision logic based on a series of business rules. 
+// The business rules specify the access that will be granted to users based on their role-based permissions and their career level. 
+// Code branches will display a different message to the user depending on their permissions and level.
+
+string permission = "Admin|Manager";
+int level = 53;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
