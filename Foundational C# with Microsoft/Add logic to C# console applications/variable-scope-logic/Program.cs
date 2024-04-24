9814436c9a28d2e9b8214ad92d840cb1faa2368a
Muhammad Sameem <sameemgujjar123@yahoo.com>
2024-04-24 09:57:12 +0400

    :heavy_check_mark: exercise variable scope logic

---
+++ Foundational C# with Microsoft/Add logic to C# console applications/variable-scope-logic/Program.cs
@@ -0,0 +1,28 @@
+﻿// Exercise - Code blocks and variable scope
+
+// bool flag = true;
+// int value = 0;
+
+// if (flag)
+// {
+//     Console.WriteLine($"Inside the code block: {value}");
+// }
+
+// value = 10;
+// Console.WriteLine($"Outside the code block: {value}");
+
+
+// Exercise - Remove code blocks from if statements
+
+bool flag = true;
+if (flag)
+    Console.WriteLine(flag);
+
+string name = "steve";
+
+if (name == "bob")
+    Console.WriteLine("Found Bob");
+else if (name == "steve")
+    Console.WriteLine("Found Steve");
+else
+    Console.WriteLine("Found Chuck");
