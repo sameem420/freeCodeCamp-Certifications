151ef5dde284fc09a1ee3e3c8f0b2efd6c1e4160
Muhammad Sameem <sameemgujjar123@yahoo.com>
2026-07-21 18:03:16 +0400

    ✔️  cleaning up

--- Foundational C# with Microsoft/Create and run simple C# console applications/variable-names/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// example of good variable names
-
-char userOption;
-
-int gameScore;
-
-float particlesPerMillion;
-
-bool processedCustomer;
--- Foundational C# with Microsoft/Write your first code using C#/write-your-first-code/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// Console.WriteLine("Hello World!");
-
-Console.Write("Congratulations!");
-Console.Write(" ");
-Console.Write("You wrote your first lines of code.");
